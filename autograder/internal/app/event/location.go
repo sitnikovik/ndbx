cd2ed882fc4e0f76af7814c92c730a76c0a7943e
Ilya Sitnikov <sitnikovik@gmail.com>
2026-03-20 21:43:28 +0300

    feat(app): city of the event location

--- autograder/internal/app/event/location.go
+++ autograder/internal/app/event/location.go
@@ -4,16 +4,37 @@ package event
 type Location struct {
 	// addr is the address of the event location.
 	addr string
+	// city is the city where the event is happening.
+	city string
+}
+
+// LocationOption represents a functional option for configuring a Location of the event.
+type LocationOption func(l *Location)
+
+// WithCity sets the provided city to the Location.
+func WithCity(city string) LocationOption {
+	return func(l *Location) {
+		l.city = city
+	}
 }
 
 // NewLocation creates a new Location instance.
-func NewLocation(addr string) Location {
-	return Location{
+func NewLocation(addr string, opts ...LocationOption) Location {
+	l := Location{
 		addr: addr,
 	}
+	for _, opt := range opts {
+		opt(&l)
+	}
+	return l
 }
 
 // Address returns the address of the event location.
 func (l Location) Address() string {
 	return l.addr
+}
+
+// City return the city where the event is happening.
+func (l Location) City() string {
+	return l.city
 }
--- autograder/internal/app/event/location_test.go
+++ autograder/internal/app/event/location_test.go
@@ -46,3 +46,52 @@ func TestLocation_Address(t *testing.T) {
 		})
 	}
 }
+
+func TestLocation_City(t *testing.T) {
+	t.Parallel()
+	type want struct {
+		val string
+	}
+	tests := []struct {
+		name string
+		l    event.Location
+		want want
+	}{
+		{
+			name: "ok",
+			l: event.NewLocation(
+				"City, Country, Street, 123",
+				event.WithCity("New York"),
+			),
+			want: want{
+				val: "New York",
+			},
+		},
+		{
+			name: "empty city but full address",
+			l: event.NewLocation(
+				"City, Country, Street, 123",
+			),
+			want: want{
+				val: "",
+			},
+		},
+		{
+			name: "default",
+			l:    event.NewLocation(""),
+			want: want{
+				val: "",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(
+				t,
+				tt.want.val,
+				tt.l.City(),
+			)
+		})
+	}
+}
