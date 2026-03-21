c0adde1ddc3f70579a40359853b052ee0ceb2b5f
Ilya Sitnikov <sitnikovik@gmail.com>
2026-03-21 14:28:21 +0300

    feat(event): add category to content

--- autograder/internal/app/event/content_test.go
+++ autograder/internal/app/event/content_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/sitnikovik/ndbx/autograder/internal/app/event"
+	"github.com/sitnikovik/ndbx/autograder/internal/app/event/category"
 )
 
 func TestContent_Title(t *testing.T) {
@@ -89,6 +90,59 @@ func TestContent_Description(t *testing.T) {
 				t,
 				tt.want.val,
 				tt.c.Description(),
+			)
+		})
+	}
+}
+
+func TestContent_Category(t *testing.T) {
+	t.Parallel()
+	type want struct {
+		val category.Type
+	}
+	tests := []struct {
+		name string
+		c    event.Content
+		want want
+	}{
+		{
+			name: "meetup",
+			c: event.NewContent(
+				"Test Event",
+				"This is a test event.",
+				event.WithCategory(
+					category.Meetup,
+				),
+			),
+			want: want{
+				val: category.Meetup,
+			},
+		},
+		{
+			name: "not set",
+			c: event.NewContent(
+				"Test Event",
+				"This is a test event.",
+			),
+			want: want{
+				val: category.Other,
+			},
+		},
+		{
+			name: "default value",
+			c:    event.Content{},
+			want: want{
+				val: category.Other,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(
+				t,
+				tt.want.val,
+				tt.c.Category(),
 			)
 		})
 	}
