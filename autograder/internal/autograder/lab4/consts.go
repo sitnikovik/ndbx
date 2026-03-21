735c1490763c1dc328f6b1707f09573bf9fa567f
Ilya Sitnikov <sitnikovik@gmail.com>
2026-03-21 13:51:43 +0300

    docs(lab-4): fix typo

--- autograder/internal/autograder/lab4/consts.go
+++ autograder/internal/autograder/lab4/consts.go
@@ -3,6 +3,6 @@ package lab4
 const (
 	// MinShardCount is the minimum number of shards required for the lab.
 	MinShardCount = 2
-	// MinReplicas is the minimum number of replicas required for each shard.
+	// MinReplicasCount is the minimum number of replicas required for each shard.
 	MinReplicasCount = 3
 )
