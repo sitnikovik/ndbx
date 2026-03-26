1ffa7c63636e3878e2be8a3c8fe9380487e12943
Ilya Sitnikov <sitnikovik@gmail.com>
2026-03-26 16:41:36 +0300

    feat(lab-4): step to get nx event

---
+++ autograder/internal/autograder/lab4/job/events/one/not-found/endpoint/doc.go
@@ -0,0 +1,4 @@
+// Package endpoint provides functionality
+// to run autograder test step to validate behavior of the target application
+// on non-existent event requested.
+package endpoint
---
+++ autograder/internal/autograder/lab4/job/events/one/not-found/endpoint/run.go
@@ -0,0 +1,42 @@
+package endpoint
+
+import (
+	"context"
+	"errors"
+
+	"github.com/sitnikovik/ndbx/autograder/internal/app/endpoint"
+	"github.com/sitnikovik/ndbx/autograder/internal/errs"
+	"github.com/sitnikovik/ndbx/autograder/internal/expect/http/response"
+	"github.com/sitnikovik/ndbx/autograder/internal/step"
+)
+
+// Run tries to get non-existent event
+// and validates the response got by the endpoint.
+func (s *Step) Run(
+	_ context.Context,
+	_ step.Variables,
+) error {
+	rsp, err := s.cli.Get(
+		endpoint.
+			NewEndpoint(s.baseURL).
+			Event("2ip3ue32-9ejnojkdsdp932u8eji"),
+	)
+	if err != nil {
+		return errors.Join(
+			errs.ErrHTTPFailed,
+			err,
+		)
+	}
+	defer errs.MustBeClosed(
+		rsp.Body.Close(),
+	)
+	err = response.AssertAll(
+		rsp,
+		response.AssertNotFoundStatus,
+		response.AssertEmptyContent,
+	)
+	if err != nil {
+		return errs.Wrap(err, "got unexpected response")
+	}
+	return nil
+}
