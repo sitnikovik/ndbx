79aaa43be276a950a517c396359c161edae4cd99
Ilya Sitnikov <sitnikovik@gmail.com>
2026-03-26 18:18:03 +0300

    feat(user): add username to identity

---
+++ autograder/internal/app/user/identity_test.go
@@ -0,0 +1,58 @@
+package user_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/sitnikovik/ndbx/autograder/internal/app/user"
+)
+
+func TestIdentity_Username(t *testing.T) {
+	t.Parallel()
+	type want struct {
+		val string
+	}
+	tests := []struct {
+		name string
+		i    user.Identity
+		want want
+	}{
+		{
+			name: "ok",
+			i: user.NewIdentity(
+				user.NewID("1"),
+				user.WithUsername("username"),
+			),
+			want: want{
+				val: "username",
+			},
+		},
+		{
+			name: "only id",
+			i: user.NewIdentity(
+				user.NewID("1"),
+			),
+			want: want{
+				val: "",
+			},
+		},
+		{
+			name: "default value",
+			i:    user.Identity{},
+			want: want{
+				val: "",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(
+				t,
+				tt.want.val,
+				tt.i.Username(),
+			)
+		})
+	}
+}
