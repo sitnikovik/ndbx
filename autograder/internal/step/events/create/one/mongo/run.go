70b537bad373c9e4e2412804fdf01d0f4f99781d
Ilya Sitnikov <sitnikovik@gmail.com>
2026-03-27 23:14:00 +0300

    feat(step): create event by mongo

---
+++ autograder/internal/step/events/create/one/mongo/run.go
@@ -0,0 +1,30 @@
+package mongo
+
+import (
+	"context"
+
+	"github.com/sitnikovik/ndbx/autograder/internal/app/mongo/event/collection"
+	appdoc "github.com/sitnikovik/ndbx/autograder/internal/app/mongo/event/doc"
+	"github.com/sitnikovik/ndbx/autograder/internal/errs"
+	"github.com/sitnikovik/ndbx/autograder/internal/step"
+)
+
+// Run inserts an event into MongoDB.
+func (s *Step) Run(
+	ctx context.Context,
+	_ step.Variables,
+) error {
+	err := s.mongo.Insert(
+		ctx,
+		collection.Name,
+		appdoc.FromEvent(s.event).KVs(),
+	)
+	if err != nil {
+		return errs.WrapJoin(
+			"failed to insert",
+			err,
+			errs.ErrExternalDependencyFailed,
+		)
+	}
+	return nil
+}
