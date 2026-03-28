dd25ac8bcce822bac96f5b1b8df8e063da49874a
Ilya Sitnikov <sitnikovik@gmail.com>
2026-03-28 11:15:18 +0300

    feat(event): copy with
    Added `CopyWith()` for the event to copy the instance in to the new one with specified values in the options

--- autograder/internal/app/event/option.go
+++ autograder/internal/app/event/option.go
@@ -1,7 +1,23 @@
 package event
 
+import "github.com/sitnikovik/ndbx/autograder/internal/app/user"
+
 // Option represents a functional option for configuring an Event.
 type Option func(*Event)
+
+// WithID sets the unique identifier for the event.
+func WithID(id ID) Option {
+	return func(e *Event) {
+		e.id = id
+	}
+}
+
+// sets the user's identification for the event.
+func WithCreatedBy(id user.Identity) Option {
+	return func(e *Event) {
+		e.created.by = id
+	}
+}
 
 // WithQuantity sets the quantity of attendees for the event in the Event.
 func WithQuantity(q Quantity) Option {
