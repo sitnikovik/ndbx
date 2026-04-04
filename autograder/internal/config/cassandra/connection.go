f7700f4108e6e77d763991672d9677c1be2ea0e5
Ilya Sitnikov <sitnikovik@gmail.com>
2026-04-04 17:06:01 +0300

    feat(cassandra): config validation

--- autograder/internal/config/cassandra/connection.go
+++ autograder/internal/config/cassandra/connection.go
@@ -1,5 +1,9 @@
 package cassandra
 
+import (
+	"errors"
+)
+
 // Connection defines a connection parameters to Apache Cassandra.
 type Connection struct {
 	// hosts is the list of Cassandra hosts.
@@ -27,4 +31,16 @@ func (c Connection) Port() int {
 // Hosts returns the list of Cassandra hosts we should connect to.
 func (c Connection) Hosts() []string {
 	return c.hosts
+}
+
+// Validate validates the connection config
+// and returns if it has any invalid value.
+func (c Connection) Validate() error {
+	if len(c.Hosts()) == 0 {
+		return errors.New("empty hosts")
+	}
+	if p := c.Port(); p == 0 || p > 99999 {
+		return errors.New("invalid port")
+	}
+	return nil
 }
--- autograder/internal/config/cassandra/database.go
+++ autograder/internal/config/cassandra/database.go
@@ -1,6 +1,10 @@
 package cassandra
 
-import "github.com/sitnikovik/ndbx/autograder/internal/client/cassandra/consistency"
+import (
+	"errors"
+
+	"github.com/sitnikovik/ndbx/autograder/internal/client/cassandra/consistency"
+)
 
 // Database is a Cassandra database configuration.
 type Database struct {
@@ -29,4 +33,16 @@ func (d Database) Keyspace() string {
 // Consistency returns the consistency level for Cassandra queries.
 func (d Database) Consistency() consistency.Consistency {
 	return d.consistency
+}
+
+// Validate validates the connection config
+// and returns if it has any invalid value.
+func (d Database) Validate() error {
+	if d.Keyspace() == "" {
+		return errors.New("empty keyspace")
+	}
+	if d.Consistency().String() == "" {
+		return errors.New("unknown consistency level")
+	}
+	return nil
 }
