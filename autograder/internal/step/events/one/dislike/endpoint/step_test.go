3d01f30617148f1b3fe1b42d9627db76af5fb613
Ilya Sitnikov <sitnikovik@gmail.com>
2026-04-09 21:17:30 +0300

    feat(step): add step to dislike event by endpoint
    - Added step to like the provided event by the endpoint
    - Event id in step variables is required to match the event in the testing app
    - Checked for valid http response
    - Checked for session in the response

---
+++ autograder/internal/step/events/one/dislike/endpoint/step_test.go
@@ -0,0 +1,89 @@
+package endpoint_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	impl "github.com/sitnikovik/ndbx/autograder/internal/step/events/one/dislike/endpoint"
+	httpxfk "github.com/sitnikovik/ndbx/autograder/internal/test/fake/client/httpx"
+	eventfx "github.com/sitnikovik/ndbx/autograder/internal/test/fixture/app/event"
+)
+
+func TestStep_Name(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name      string
+		step      *impl.Step
+		want      string
+		wantPanic bool
+	}{
+		{
+			name: "ok",
+			step: impl.NewStep(
+				httpxfk.NewFakeClient(),
+				"/localhost",
+				eventfx.NewTestEvent(),
+			),
+			want: impl.Name,
+		},
+		{
+			name: "default fields",
+			step: impl.NewStep(
+				nil,
+				"",
+				eventfx.NewTestEvent(),
+			),
+			want: impl.Name,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(
+				t,
+				tt.want,
+				tt.step.Name(),
+			)
+		})
+	}
+}
+
+func TestStep_Description(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name      string
+		step      *impl.Step
+		want      string
+		wantPanic bool
+	}{
+		{
+			name: "ok",
+			step: impl.NewStep(
+				httpxfk.NewFakeClient(),
+				"/localhost",
+				eventfx.NewTestEvent(),
+			),
+			want: impl.Description,
+		},
+		{
+			name: "default fields",
+			step: impl.NewStep(
+				nil,
+				"",
+				eventfx.NewTestEvent(),
+			),
+			want: impl.Description,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(
+				t,
+				tt.want,
+				tt.step.Description(),
+			)
+		})
+	}
+}
