d4c29a418c4915c3180bd86637c19de61b9e8911
Ilya Sitnikov <sitnikovik@gmail.com>
2026-04-28 08:11:48 +0300

    feat(step): impl expectations for user event reviews

--- autograder/internal/step/user/one/events/expect/expect.go
+++ autograder/internal/step/user/one/events/expect/expect.go
@@ -2,12 +2,15 @@ package expect
 
 import (
 	"github.com/sitnikovik/ndbx/autograder/internal/app/event/reaction"
+	"github.com/sitnikovik/ndbx/autograder/internal/app/event/review"
 )
 
 // Expectations holds the expectations we need to check in the Step.
 type Expectations struct {
 	// reactions is the event reactions for each event to expect.
 	reactions []reaction.Reactions
+	// reviews is the event reviews for each event to expect.
+	reviews []review.Reviews
 }
 
 // NewExpectations creates a new Expectations instance.
@@ -28,4 +31,14 @@ func (e Expectations) HasReactions() bool {
 // Reactions returns event Reactions to expect.
 func (e Expectations) Reactions() []reaction.Reactions {
 	return e.reactions
+}
+
+// HasReviews defines are reviews set in the Expectations instance.
+func (e Expectations) HasReviews() bool {
+	return e.reviews != nil
+}
+
+// Reviews returns event Reviews to expect.
+func (e Expectations) Reviews() []review.Reviews {
+	return e.reviews
 }
--- autograder/internal/step/user/one/events/expect/option.go
+++ autograder/internal/step/user/one/events/expect/option.go
@@ -2,6 +2,7 @@ package expect
 
 import (
 	"github.com/sitnikovik/ndbx/autograder/internal/app/event/reaction"
+	"github.com/sitnikovik/ndbx/autograder/internal/app/event/review"
 )
 
 // Option represents the functional option
@@ -14,5 +15,21 @@ type Option func(e *Expectations)
 func WithReactions(rr []reaction.Reactions) Option {
 	return func(e *Expectations) {
 		e.reactions = rr
+	}
+}
+
+// WithReviews sets event reviews to the Expectations instance on creation.
+//
+// The list is supposed to match the length of events to expect.
+func WithReviews(rr ...review.Reviews) Option {
+	return func(e *Expectations) {
+		e.reviews = rr
+	}
+}
+
+// WithNoReviews sets that there is no any event reviews in response to expect.
+func WithNoReviews() Option {
+	return func(e *Expectations) {
+		e.reviews = []review.Reviews{}
 	}
 }
