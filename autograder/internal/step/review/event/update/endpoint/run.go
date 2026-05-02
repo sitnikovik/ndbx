56efc4b3a5708adfccff7d3b4d3a5741e372e63b
Ilya Sitnikov <sitnikovik@gmail.com>
2026-05-02 22:08:46 +0300

    feat(step): update review by endpoint

---
+++ autograder/internal/step/review/event/update/endpoint/run.go
@@ -0,0 +1,55 @@
+package endpoint
+
+import (
+	"bytes"
+	"context"
+	"errors"
+
+	"github.com/sitnikovik/ndbx/autograder/internal/app/endpoint"
+	"github.com/sitnikovik/ndbx/autograder/internal/errs"
+	"github.com/sitnikovik/ndbx/autograder/internal/step"
+)
+
+// Run updates the event review and validates the response.
+func (s *Step) Run(
+	_ context.Context,
+	vars step.Variables,
+) error {
+	rsp, err := s.cli.Patch(
+		endpoint.
+			NewEndpoint(s.baseURL).
+			EventReviews(
+				vars.
+					MustGet(s.event.Hash()).
+					AsString(),
+			),
+		bytes.NewBuffer(
+			s.rq.MustBytes(),
+		),
+	)
+	if err != nil {
+		return errors.Join(
+			errs.ErrHTTPFailed,
+			err,
+		)
+	}
+	defer func() {
+		errs.MustBeClosed(
+			rsp.Body.Close(),
+		)
+	}()
+	defer func() {
+		errs.MustBeClosed(
+			rsp.Body.Close(),
+		)
+	}()
+	err = s.want.Assert(rsp)
+	if err != nil {
+		return errs.WrapJoin(
+			"unexpected response",
+			errs.ErrExpectationFailed,
+			err,
+		)
+	}
+	return nil
+}
