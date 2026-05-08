77d39cf02b6c7bbec3f939ed9b04d7b838fc00d7
Ilya Sitnikov <sitnikovik@gmail.com>
2026-05-08 22:05:03 +0300

    feat(event): add equals method for costs

--- autograder/internal/app/event/costs_test.go
+++ autograder/internal/app/event/costs_test.go
@@ -56,3 +56,105 @@ func TestCosts_Entry(t *testing.T) {
 		})
 	}
 }
+
+func TestCosts_Equals(t *testing.T) {
+	t.Parallel()
+	type args struct {
+		other event.Costs
+	}
+	type want struct {
+		value bool
+	}
+	tests := []struct {
+		name string
+		c    event.Costs
+		args args
+		want want
+	}{
+		{
+			name: "same entry",
+			c: event.NewCosts(
+				money.NewMoney(100, 50),
+			),
+			args: args{
+				other: event.NewCosts(
+					money.NewMoney(100, 50),
+				),
+			},
+			want: want{
+				value: true,
+			},
+		},
+		{
+			name: "diff entry",
+			c: event.NewCosts(
+				money.NewMoney(100, 50),
+			),
+			args: args{
+				other: event.NewCosts(
+					money.NewMoney(100, 00),
+				),
+			},
+			want: want{
+				value: false,
+			},
+		},
+		{
+			name: "default value",
+			c:    event.Costs{},
+			args: args{
+				other: event.NewCosts(
+					money.NewMoney(100, 00),
+				),
+			},
+			want: want{
+				value: false,
+			},
+		},
+		{
+			name: "default arg",
+			c: event.NewCosts(
+				money.NewMoney(100, 00),
+			),
+			args: args{
+				other: event.Costs{},
+			},
+			want: want{
+				value: false,
+			},
+		},
+		{
+			name: "default value",
+			c:    event.Costs{},
+			args: args{
+				other: event.NewCosts(
+					money.NewMoney(100, 00),
+				),
+			},
+			want: want{
+				value: false,
+			},
+		},
+		{
+			name: "default arg and value",
+			c:    event.Costs{},
+			args: args{
+				other: event.Costs{},
+			},
+			want: want{
+				value: true,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := tt.c.Equals(tt.args.other)
+			if tt.want.value {
+				assert.True(t, got)
+			} else {
+				assert.False(t, got)
+			}
+		})
+	}
+}
