308eddcacc60a5faad37d41994c116ceeb19abb9
Ilya Sitnikov <sitnikovik@gmail.com>
2026-05-13 20:31:29 +0300

    feat(config): validate neo4j connection

--- autograder/internal/config/neo4j/connection.go
+++ autograder/internal/config/neo4j/connection.go
@@ -1,5 +1,7 @@
 package neo4j
 
+import "errors"
+
 // Connection is the Neo4j database connection configuration.
 type Connection struct {
 	// url is the Neo4j database URL
@@ -16,4 +18,14 @@ func NewConnection(u string) Connection {
 // URL returns the Neo4j database URL.
 func (c Connection) URL() string {
 	return c.url
+}
+
+// Validate validates the connection configuration.
+//
+// Not validates the URL format so if the URL is invalid, nil is returned.
+func (c Connection) Validate() error {
+	if c.url == "" {
+		return errors.New("empty url")
+	}
+	return nil
 }
--- autograder/internal/config/neo4j/connection_test.go
+++ autograder/internal/config/neo4j/connection_test.go
@@ -57,3 +57,70 @@ func TestConnection_URL(t *testing.T) {
 		})
 	}
 }
+
+func TestConnection_Validate(t *testing.T) {
+	t.Parallel()
+	type want struct {
+		errContains string
+		errored     bool
+	}
+	tests := []struct {
+		name string
+		c    impl.Connection
+		want want
+	}{
+		{
+			name: "ok",
+			c:    impl.NewConnection("neo4j://localhost:7687"),
+			want: want{
+				errContains: "",
+				errored:     false,
+			},
+		},
+		{
+			name: "empty",
+			c:    impl.NewConnection(""),
+			want: want{
+				errContains: "url",
+				errored:     true,
+			},
+		},
+		{
+			name: "invalid",
+			c:    impl.NewConnection("invalid"),
+			want: want{
+				errContains: "",
+				errored:     false,
+			},
+		},
+		{
+			name: "default value",
+			c:    impl.Connection{},
+			want: want{
+				errContains: "url",
+				errored:     true,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			err := tt.c.Validate()
+			if tt.want.errored {
+				assert.Error(t, err)
+				assert.Contains(
+					t,
+					err.Error(),
+					tt.want.errContains,
+				)
+			} else {
+				assert.NoErrorf(
+					t,
+					err,
+					"unexpected error: %v",
+					err,
+				)
+			}
+		})
+	}
+}
