d658bf55e4f429bcbc26451840c68ee6d785dff8
Ilya Sitnikov <sitnikovik@gmail.com>
2026-05-16 15:35:05 +0300

    feat(app): add neo4j user
    Added `User{}` represents the user stored in Neo4j of the target application

---
+++ autograder/internal/app/neo4j/user/user_test.go
@@ -0,0 +1,67 @@
+package user_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	impl "github.com/sitnikovik/ndbx/autograder/internal/app/neo4j/user"
+	"github.com/sitnikovik/ndbx/autograder/internal/app/user"
+)
+
+func TestUser_ID(t *testing.T) {
+	t.Parallel()
+	type want struct {
+		value user.ID
+	}
+	tests := []struct {
+		name string
+		u    impl.User
+		want want
+	}{
+		{
+			name: "ok",
+			u: impl.NewUser(
+				user.NewID("65e9c0b1a2b3c4d5e6f7a8b9"),
+			),
+			want: want{
+				value: user.NewID("65e9c0b1a2b3c4d5e6f7a8b9"),
+			},
+		},
+		{
+			name: "empty id",
+			u: impl.NewUser(
+				user.NewID(""),
+			),
+			want: want{
+				value: user.NewID(""),
+			},
+		},
+		{
+			name: "whitespace id",
+			u: impl.NewUser(
+				user.NewID("   "),
+			),
+			want: want{
+				value: user.NewID("   "),
+			},
+		},
+		{
+			name: "default value",
+			u:    impl.User{},
+			want: want{
+				value: user.NewID(""),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(
+				t,
+				tt.want.value,
+				tt.u.ID(),
+			)
+		})
+	}
+}
